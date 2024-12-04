13256b8d2716956c44ac427c18ab1cb273e1c6c7
Stefanel Mihaila <stefanel.mihaila@yahoo.com>
2024-12-04 18:29:18 +0100

    feat: pari e dispari

--- Pari e Dispari/script.js
+++ Pari e Dispari/script.js
@@ -14,41 +14,67 @@
 // 2 - Dichiariamo chi ha vinto
 
 
-
+// --- Function per sommare due numeri num1 + num2
 function sumNumbers(num1, num2) {
     return  num1 + num2;
 }
 
-const numbUtente = Number(prompt(`Inserisci un numero da 1 a 5`))
-console.log(numbUtente)
+// --- Function genera numero random CPU
 
-if ( numbUtente >= 1 && numbUtente <= 5) {  
+function generaNumeroRandom(min, max) {
+    const random = Math.floor(Math.random() * (max - min + 1)) + min;
+    return random;
 }
-else {
-    alert(`Hai inserito un numero sbagliato`)
+
+ // --- Function per controlare se un numero e pari o dispari
+ function evenOodd(sum) {
+    if ( sum % 2 === 0 ) {
+        alert(`la somma e un numero pari ${sum} 😁`)
+        return true
+    }
+    else {
+        alert(`la somma e un numero dispari ${sum} 😁`)
+    }
+    return false
+}
+
+// --- Function controllo se due numeri sono pari entrambi, o se due numeri sono dispari entrambim o se un numero e pari e un numero dispari o al contrario
+function numbersEvenorOdd(num1, num2) {
+    if ( numbUtente % 2 === 0 && sum % 2 === 0 ) {
+        alert(`Il numero del utente e pari ${numbUtente} e il numero della somma e pari ${sum} quindi ha vinto il utente perche tutti due numeri sono pari 😎`)
+        return true
+    }
+    else if ( numbUtente % 2 === 1 && sum % 2 === 1) {
+        alert(`Il numero del utente e dispari ${numbUtente} e il numero della somma e dispari ${sum} quindi ha vinto il utente perche tutti due numeri sono dispari 😎`)
+        return true
+
+    }
+    else {
+        alert(`Ha vinto il CPU perche il numero del utente ${numbUtente} e diverso dal numero ${sum} pari/dispari o dispari/pari 😢`)
+    }
+    return false
+}
+
+// --- Function per chiedere al utente di inserire un numero 
+function numbUser(min, max) {
+    const numbUtente = Number(prompt(`Inserisci un numero da 1 a 5`))
+    console.log(numbUtente)
+    if ( numbUtente >= min && numbUtente <= max) {  
+    }
+    else {
+    alert(`Hai inserito un numero sbagliato 😢`)
     broke;
+    }
+    return numbUtente
 }
 
-const numbCpu = Math.floor(Math.random() * 4) + 1;
-console.log(numbCpu)
 
-
+const numbCpu = generaNumeroRandom(1, 5)
+console.log(numbCpu);
+const numbUtente = numbUser(1, 5);
 const sum = sumNumbers(numbUtente, numbCpu)
 console.log(sum)
-
-if ( sum % 2 === 0 ) {
-    alert(`i numeri sono pari`)
-}
-else {
-    alert(`i numeri sono dispari`)
-}
-
-if ( numbUtente % 2 === 0 && sum % 2 === 0 ) {
-    alert(`i numeri sono pari tutte due e ha vinto l'utente`)
-}
-else if ( numbUtente % 2 === 1 && sum % 2 === 1) {
-    alert(`i numeri sono dispari tutte due e ha vinto l'utente`)
-}
-else {
-    alert(`i numeri uno e pari e l'altro dispari e quindi ha vinto CPU`)
-}
+const even = evenOodd(sum);
+console.log(even)
+const isEvenorOdd = numbersEvenorOdd(numbUtente, sum)
+console.log(isEvenorOdd);
