1406afb70bdfc14d3e16376f7b6385fc5ca63d97
Stefanel Mihaila <stefanel.mihaila@yahoo.com>
2024-12-04 16:13:04 +0100

    feat: ex Palindroma

--- Palindroma/script.js
+++ Palindroma/script.js
@@ -2,3 +2,26 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
 
+// 1 - Chiedo all'utente di inserire una parola
+// 2 - Salvo la parola del utente scritta al reverso in una nuova varibile
+// 3 - Controllo se la parola inserita del utente e palindroma
+
+
+    
+ function reverseWord(word) {
+        let wordReverse =""
+        for (let i = word.length - 1; i >= 0; i--) {
+            wordReverse += word[i];
+        }
+
+          if (wordReverse === word) {
+            return true
+          }
+        
+        return false
+ }
+
+
+
+ const word = prompt(`Inserisci una parola`)
+ console.log(reverseWord(word))
